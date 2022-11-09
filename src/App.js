567bc3838edb098b184d930b50f3509ce3b22c03
mohseen1985 <ghorbani.mohsenn@gmail.com>
2022-11-09 13:59:00 +0330

    get data from api and log data in console

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+
+
+//Components
+import Landing from "./Components/shared/Landing";
+function App() {
+  return (
+
+      <div className="w-full bg-gray-100 min-h-screen flex flex-col">
+        <Landing />
+      </div>
+
+  );
+}
+
+export default App;
